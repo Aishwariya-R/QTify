c2ba305b90ae95bd1f6641be7325cc1417f0440e
Aishwariya <aishurks59@gmail.com>
2024-09-19 20:49:16 +0530

    Final code

--- src/index.js
+++ src/index.js
@@ -2,14 +2,35 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import HomePage from './pages/HomePage/HomePage';
+import AlbumPage from './pages/AlbumPage/AlbumPage';
 //import reportWebVitals from './reportWebVitals';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    children: [
+      {
+      path: "/",
+      element: <HomePage />
+      },
+      {
+        path: "/album/:albumId",
+        element: <AlbumPage />
+      }
+    ]
+  },
+]);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+  <RouterProvider router={router}/>);
+  // <React.StrictMode>
+  //   <App />
+  // </React.StrictMode>
+
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
